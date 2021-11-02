d1844ab4032780fb228fafd47d8c2bc552be2f90
aycaateser <190315075@ogr.cbu.edu.tr>
2021-11-02 18:33:32 +0300

    week2/second commit

--- week2/CleanBlog/app.js
+++ week2/CleanBlog/app.js
@@ -1,14 +1,28 @@
 const express=require('express')
+const ejs=require('ejs')
+const path=require('path')
 const app=express();
-app.get('/',(req,res)=>{
+//template engine
+app.set("view engine","ejs");
 
-    const blog={
-        id:1,
-        title:"blog title",
-        description:"blog description"
-    }
-    res.send(blog)
-})
+//MIDDLEWARE(req-response arasındaki hersey) 
+app.use(express.static('public')) //public folder for static files
+
+//Routing
+app.get('/index.html',(req,res)=>{
+    res.render("index")
+});
+app.get('/about.html',(req,res)=>{
+    res.render("about")
+});
+app.get('/add_post.html',(req,res)=>{
+    res.render("add_post")
+});
+
+
+
+
+
 const port=5000;
 app.listen(port,()=>{
     console.log(`server started on port ${port}..`)
