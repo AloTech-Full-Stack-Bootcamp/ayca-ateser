c6bc5937b9fb1c6228e168e231284fcccaec4a81
aycaateser <190315075@ogr.cbu.edu.tr>
2021-11-05 22:30:49 +0300

    ready for heroku

--- week2/CleanBlog/app.js
+++ week2/CleanBlog/app.js
@@ -36,8 +36,12 @@ app.get("/about", pageController.getAboutPage);
 app.get("/add", pageController.getAddPage);
 
 //port settingss
-const port=5000;
+/*const port=5000;
 app.listen(port,()=>{
     console.log(`server started on port ${port}..`)
 });
-
+*/
+const port=process.env.PORT||5000;
+app.listen(port,()=>{
+    console.log(`server started on ${port} port..`);
+});
